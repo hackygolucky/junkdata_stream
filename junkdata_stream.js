53e59c63521dc8bab051d800727a9604133e0fdc
Tracy Abrahms <tracyabrahms@gmail.com>
2013-03-28 23:05:37 -0700

    added mapping logic

--- junkdata_stream.js
+++ junkdata_stream.js
@@ -1,15 +1,35 @@
 // Junk data stream
-var junkdata_server = require('/junkdata-server')(33, {keyname: Number})
- 
- through(function write(data) {
-	this.queue(data) 
-	for (key in junkdata_server) {
-		junkdata_server.key = data
-	}
+var through = ('through')
 
+
+
+function template_mapping(template) {
+	var result = {}
+	for (key in result) {
+		if (template[key] === String) {
+			result[key] = Math.random().toString()
+		} else if (template[key] === Boolean) {
+			result[key] = Math.random() > 0.5
+		} else if (template[key] == Number) {
+			result[key] = Math.random()
+		}
+	}
+	return result
+}
+
+function myStream(ttl, mappings) {
+	// returns a stream
+	return through(function write(data) {
+		this.queue(data)
 	}, 
+
 	function end() {
 		this.queue(null) // do we want this to end? This is optional.
 	}
+	}
 
-junkdata_server.listen(8000)
+myStream.on('data', console.log)
+
+
+
+
